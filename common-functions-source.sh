ee017c0a81d4534ae0b54cc43d246c54c6c553cc
Liviu Ionescu <ilg@livius.net>
2019-03-07 18:24:41 +0200

    move code to common-functions

--- common-functions-source.sh
+++ common-functions-source.sh
@@ -72,6 +72,7 @@ function prepare_prerequisites()
   elif [ "${TARGET_PLATFORM}" == "linux" ]
   then
 
+# ??????
 if false
 then
 
@@ -106,11 +107,15 @@ fi
     DOWNLOAD_FOLDER_PATH="${HOST_CACHE_FOLDER_PATH}"
   fi
 
-  if [ \( "${IS_DEVELOP}" != "y" \) -a \( -f "/.dockerenv" \) ]
-  then
-    BUILD_FOLDER_PATH="/tmp/${TARGET_FOLDER_NAME}/build"
-  else
-    BUILD_FOLDER_PATH="${WORK_FOLDER_PATH}/${TARGET_FOLDER_NAME}/build"
+  # Develop builds use the host folder.
+  BUILD_FOLDER_PATH="${WORK_FOLDER_PATH}/${TARGET_FOLDER_NAME}/build"
+  if [ -f "/.dockerenv" ]
+  then 
+    if [ "${IS_DEVELOP}" != "y" ]
+    then
+      # Docker builds use a temporary folder.
+      BUILD_FOLDER_PATH="/tmp/${TARGET_FOLDER_NAME}/build"
+    fi
   fi
 
   LIBS_BUILD_FOLDER_PATH="${BUILD_FOLDER_PATH}/libs"
@@ -126,13 +131,12 @@ fi
   mkdir -p "${LIBS_INSTALL_FOLDER_PATH}"
   mkdir -p "${APP_INSTALL_FOLDER_PATH}"
 
-  APP_PREFIX="${APP_INSTALL_FOLDER_PATH}"
-  APP_PREFIX_DOC="${APP_PREFIX}"/doc
-
   DEPLOY_FOLDER_NAME=${DEPLOY_FOLDER_NAME:-"deploy"}
   DEPLOY_FOLDER_PATH="${WORK_FOLDER_PATH}/${DEPLOY_FOLDER_NAME}"
   # Do it later, only if needed.
   # mkdir -p "${DEPLOY_FOLDER_PATH}"
+
+  IS_DEVELOP=${IS_DEVELOP:-""}
 }
 
 # -----------------------------------------------------------------------------
@@ -409,6 +413,46 @@ function is_linux_sys_so()
 
 # -----------------------------------------------------------------------------
 
+# Strip binary files as in "strip binary" form, for both native
+# (linux/mac) and mingw.
+function strip_binary() 
+{
+    set +e
+    if [ $# -lt 2 ]
+    then
+        warning "strip_binary: Missing arguments"
+        exit 1
+    fi
+
+    local strip="$1"
+    local bin="$2"
+
+    if is_elf ${bin}
+    then
+      echo ${strip} ${bin}
+      ${strip} ${bin} 2>/dev/null || true
+    else
+      echo $(file ${bin})
+    fi
+
+    set -e
+}
+
+function is_elf()
+{
+  if [ $# -lt 1 ]
+  then
+    warning "is_elf: Missing arguments"
+    exit 1
+  fi
+  local bin="$1"
+
+  # Return 0 (true) if found.
+  file ${bin} | egrep -q "( ELF )|( PE )|( PE32 )|( PE32\+ )|( Mach-O )"
+}
+
+# -----------------------------------------------------------------------------
+
 function copy_win_gcc_dll() 
 {
   local dll_name="$1"
@@ -458,6 +502,420 @@ function copy_win_libwinpthread_dll()
 
 # -----------------------------------------------------------------------------
 
+function change_dylib()
+{
+  local dylib_name="$1"
+  local file_path="$2"
+
+  local dylib_path=$(otool -L "${file_path}" | grep "${dylib_name}" | sed -e 's/[[:space:]]*\(.*dylib\).*/\1/')
+
+  if [ -z "${dylib_path}" ]
+  then
+    echo "Dylib ${dylib_name} not used in binary ${file_path}..."
+    exit 1
+  fi
+
+  chmod +w "${file_path}"
+  install_name_tool \
+    -change "${dylib_path}" \
+    "@executable_path/${dylib_name}" \
+    "${file_path}"
+
+  if [ ! -f "$(dirname ${file_path})/$(basename ${dylib_path})" ]
+  then
+    cp "${dylib_path}" "$(dirname ${file_path})"
+    chmod +w "$(dirname ${file_path})/$(basename ${dylib_path})"
+  fi
+}
+
+# Workaround to Docker error on 32-bit image:
+# stat: Value too large for defined data type
+function patch_linux_elf_origin()
+{
+  local file_path="$1"
+
+  local tmp_path=$(mktemp)
+  rm -rf "${tmp_path}"
+  cp "${file_path}" "${tmp_path}"
+  patchelf --set-rpath '$ORIGIN' "${tmp_path}"
+  cp "${tmp_path}" "${file_path}"
+  rm -rf "${tmp_path}"
+}
+
+function copy_linux_user_so() 
+{
+  local dll_name="$1"
+
+  ILIB=$(find ${LIBS_INSTALL_FOLDER_PATH}/lib* -type f -name ${dll_name}'.so.*' -print | egrep '.so.[[:digit:]]+.[[:digit:]]+.[[:digit:]]+$')
+  if [ ! -z "${ILIB}" ]
+  then
+    echo "Found user ${ILIB}, 3 digits"
+
+    ihead=$(echo "${ILIB}" | head -n 1)
+    # Add "runpath" in library with value $ORIGIN.
+    patch_linux_elf_origin "${ihead}"
+
+    /usr/bin/install -v -c -m 644 "${ihead}" "${APP_PREFIX}/bin"
+
+    ILIB_BASE="$(basename ${ihead})"
+    # Skip last two digits, keep one.
+    ILIB_SHORT="$(echo $ILIB_BASE | sed -e 's/\(.*\)[.][[:digit:]]*[.][[:digit:]]*/\1/')"
+    (
+      cd "${APP_PREFIX}/bin"
+      rm --force "${ILIB_SHORT}"
+      ln -sv "${ILIB_BASE}" "${ILIB_SHORT}"
+    )
+    # Skip all three digits, keep none.
+    ILIB_SHORT="$(echo $ILIB_BASE | sed -e 's/\(.*\)[.][[:digit:]]*[.][[:digit:]]*[.][[:digit:]]*/\1/')"
+    (
+      cd "${APP_PREFIX}/bin"
+      rm --force "${ILIB_SHORT}"
+      ln -sv "${ILIB_BASE}" "${ILIB_SHORT}"
+    )
+  else
+    ILIB=$(find ${LIBS_INSTALL_FOLDER_PATH}/lib* -type f -name ${dll_name}'.so.*' -print | egrep '.so.[[:digit:]]+.[[:digit:]]+$')
+    if [ ! -z "${ILIB}" ]
+    then
+      echo "Found user ${ILIB}, 2 digits"
+
+      ihead=$(echo "${ILIB}" | head -n 1)
+      # Add "runpath" in library with value $ORIGIN.
+      patch_linux_elf_origin "${ihead}"
+
+      /usr/bin/install -v -c -m 644 "${ihead}" "${APP_PREFIX}/bin"
+
+      ILIB_BASE="$(basename ${ihead})"
+      # Skip last 1 digit, keep one.
+      ILIB_SHORT="$(echo $ILIB_BASE | sed -e 's/\(.*\)[.][[:digit:]]*/\1/')"
+      (
+        cd "${APP_PREFIX}/bin"
+        rm --force "${ILIB_SHORT}"
+        ln -sv "${ILIB_BASE}" "${ILIB_SHORT}"
+      )
+      # Skip all two digits, keep none.
+      ILIB_SHORT="$(echo $ILIB_BASE | sed -e 's/\(.*\)[.][[:digit:]]*[.][[:digit:]]*/\1/')"
+      (
+        cd "${APP_PREFIX}/bin"
+        rm --force "${ILIB_SHORT}"
+        ln -sv "${ILIB_BASE}" "${ILIB_SHORT}"
+      )
+    else
+      ILIB=$(find ${LIBS_INSTALL_FOLDER_PATH}/lib* -type f -name ${dll_name}'.so.*' -print | egrep '.so.[[:digit:]]+$')
+      if [ ! -z "${ILIB}" ]
+      then
+        echo "Found user ${ILIB}, 1 digit"
+        ihead=$(echo "${ILIB}" | head -n 1)
+        /usr/bin/install -v -c -m 644 "${ihead}" "${APP_PREFIX}/bin"
+
+        ILIB_BASE="$(basename ${ihead})"
+        # Skip final digit, keep none.
+        ILIB_SHORT="$(echo $ILIB_BASE | sed -e 's/\(.*\)[.][[:digit:]]*/\1/')"
+        echo "${ILIB_SHORT}"
+        (
+          cd "${APP_PREFIX}/bin"
+          rm --force "${ILIB_SHORT}"
+          ln -sv "${ILIB_BASE}" "${ILIB_SHORT}"
+        )
+      else
+        ILIB=$(find ${LIBS_INSTALL_FOLDER_PATH}/lib* -type f -name ${dll_name}'.so' -print)
+        if [ ! -z "${ILIB}" ]
+        then
+          echo "Found user ${ILIB}, no digits"
+          ihead=$(echo "${ILIB}" | head -n 1)
+          patch_linux_elf_origin "${ihead}"
+          /usr/bin/install -v -c -m 644 "${ihead}" "${APP_PREFIX}/bin"
+        else
+          echo ${dll_name} not found
+          exit 1
+        fi
+      fi
+    fi
+  fi
+}
+
+function copy_linux_system_so()
+{
+  local dll_name="$1"
+
+  set +e
+  local ILIB=$(find ${XBB_FOLDER}/lib* /lib* -type f -name ${dll_name}'.so.*' -print | egrep '.so.[[:digit:]]+.[[:digit:]]+.[[:digit:]]+$')
+  if [ ! -z "${ILIB}" ]
+  then
+    echo "Found system ${ILIB}, 3 digits"
+    ihead=$(echo "${ILIB}" | head -n 1)
+    /usr/bin/install -v -c -m 644 "${ihead}" "${APP_PREFIX}/bin"
+
+    ILIB_BASE="$(basename ${ihead})"
+    # Skip last two digits, keep one.
+    ILIB_SHORT="$(echo $ILIB_BASE | sed -e 's/\(.*\)[.][[:digit:]]*[.][[:digit:]]*/\1/')"
+    (
+      cd "${APP_PREFIX}/bin"
+      rm --force "${ILIB_SHORT}"
+      ln -sv "${ILIB_BASE}" "${ILIB_SHORT}"
+    )
+    # Skip all three digits, keep none.
+    ILIB_SHORT="$(echo $ILIB_BASE | sed -e 's/\(.*\)[.][[:digit:]]*[.][[:digit:]]*[.][[:digit:]]*/\1/')"
+    (
+      cd "${APP_PREFIX}/bin"
+      rm --force "${ILIB_SHORT}"
+      ln -sv "${ILIB_BASE}" "${ILIB_SHORT}"
+    )
+  else
+    ILIB=$(find ${XBB_FOLDER}/lib* /lib* -type f -name ${dll_name}'.so.*' -print | egrep '.so.[[:digit:]]+.[[:digit:]]+$')
+    if [ ! -z "${ILIB}" ]
+    then
+      echo "Found system ${ILIB}, 2 digits"
+      ihead=$(echo "${ILIB}" | head -n 1)
+      /usr/bin/install -v -c -m 644 "${ihead}" "${APP_PREFIX}/bin"
+
+      ILIB_BASE="$(basename ${ihead})"
+      # Skip last 1 digit, keep one.
+      ILIB_SHORT="$(echo $ILIB_BASE | sed -e 's/\(.*\)[.][[:digit:]]*/\1/')"
+      (
+        cd "${APP_PREFIX}/bin"
+        rm --force "${ILIB_SHORT}"
+        ln -sv "${ILIB_BASE}" "${ILIB_SHORT}"
+      )
+      # Skip all two digits, keep none.
+      ILIB_SHORT="$(echo $ILIB_BASE | sed -e 's/\(.*\)[.][[:digit:]]*[.][[:digit:]]*/\1/')"
+      (
+        cd "${APP_PREFIX}/bin"
+        rm --force "${ILIB_SHORT}"
+        ln -sv "${ILIB_BASE}" "${ILIB_SHORT}"
+      )
+    else
+      ILIB=$(find ${XBB_FOLDER}/lib* /lib* -type f -name ${dll_name}'.so.*' -print | egrep '.so.[[:digit:]]+$')
+      if [ ! -z "${ILIB}" ]
+      then
+        echo "Found system ${ILIB}, 1 digit"
+        ihead=$(echo "${ILIB}" | head -n 1)
+        /usr/bin/install -v -c -m 644 "${ihead}" "${APP_PREFIX}/bin"
+
+        ILIB_BASE="$(basename ${ihead})"
+        # Skip final digit, keep none.
+        ILIB_SHORT="$(echo $ILIB_BASE | sed -e 's/\(.*\)[.][[:digit:]]*/\1/')"
+        echo "${ILIB_SHORT}"
+        (
+          cd "${APP_PREFIX}/bin"
+          rm --force "${ILIB_SHORT}"
+          ln -sv "${ILIB_BASE}" "${ILIB_SHORT}"
+        )
+      else
+        ILIB=$(find ${XBB_FOLDER}/lib* /lib* -type f -name ${dll_name}'.so' -print)
+        if [ ! -z "${ILIB}" ]
+        then
+          echo "Found system ${ILIB}, no digits"
+          ihead=$(echo "${ILIB}" | head -n 1)
+          /usr/bin/install -v -c -m 644 "${ihead}" "${APP_PREFIX}/bin"
+        else
+          echo ${dll_name} not found
+          exit 1
+        fi
+      fi
+    fi
+  fi
+  set -e
+}
+
+
+# -----------------------------------------------------------------------------
+
+# $1 - absolute path to input folder
+# $2 - name of output folder below INSTALL_FOLDER
+function copy_license() 
+{
+  # Iterate all files in a folder and install some of them in the
+  # destination folder
+  echo
+  echo "$2"
+  (
+    cd "$1"
+    for f in *
+    do
+      if [ -f "$f" ]
+      then
+        if [[ "$f" =~ AUTHORS.*|NEWS.*|COPYING.*|README.*|LICENSE.*|FAQ.*|DEPENDENCIES.*|THANKS.* ]]
+        then
+          /usr/bin/install -d -m 0755 \
+            "${APP_PREFIX}/${DISTRO_LC_NAME}/licenses/$2"
+          /usr/bin/install -v -c -m 644 "$f" \
+            "${APP_PREFIX}/${DISTRO_LC_NAME}/licenses/$2"
+        fi
+      fi
+    done
+  )
+  (
+    xbb_activate
+
+    if [ "${TARGET_PLATFORM}" == "win32" ]
+    then
+      find "${APP_PREFIX}/${DISTRO_LC_NAME}/licenses" \
+        -type f \
+        -exec unix2dos '{}' ';'
+    fi
+  )
+}
+
+function copy_build_files()
+{
+  echo
+  echo "Copying build files..."
+
+  (
+    cd "${WORK_FOLDER_PATH}"/build.git
+
+    mkdir -p patches
+
+    find scripts patches -type d \
+      -exec /usr/bin/install -d -m 0755 \
+        "${APP_PREFIX}"/${DISTRO_LC_NAME}/'{}' ';'
+
+    find scripts patches -type f \
+      -exec /usr/bin/install -v -c -m 644 \
+        '{}' "${APP_PREFIX}"/${DISTRO_LC_NAME}/'{}' ';'
+
+    if [ -f CHANGELOG.txt ]
+    then
+      /usr/bin/install -v -c -m 644 \
+          CHANGELOG.txt "${APP_PREFIX}"/${DISTRO_LC_NAME}
+    fi
+    if [ -f CHANGELOG.md ]
+    then
+      /usr/bin/install -v -c -m 644 \
+          CHANGELOG.md "${APP_PREFIX}"/${DISTRO_LC_NAME}
+    fi
+  )
+}
+
+# -----------------------------------------------------------------------------
+
+# Copy one folder to another
+function copy_dir() 
+{
+  local from_path="$1"
+  local to_path="$2"
+
+  set +u
+  mkdir -p "${to_path}"
+
+  (cd "${from_path}" && tar cf - .) | (cd "${to_path}" && tar xf -)
+  set -u
+}
+
+# -----------------------------------------------------------------------------
+
+function create_archive()
+{
+  (
+    xbb_activate
+
+    local distribution_file_version="${RELEASE_VERSION}-${DISTRIBUTION_FILE_DATE}"
+
+    local target_folder_name=${TARGET_FOLDER_NAME}
+
+    if [ "${HAS_NAME_ARCH}" != "y" ]
+    then
+      # Temporarily use the old file name convention.
+      if [ "${TARGET_PLATFORM}" == "win32" ]
+      then
+        target_folder_name="${CONTAINER_DISTRO_LC_NAME}${TARGET_BITS}"
+      elif [ "${TARGET_PLATFORM}" == "darwin" ]
+      then
+        target_folder_name="macos"
+      elif [ "${TARGET_PLATFORM}" == "linux" ]
+      then
+        target_folder_name="${CONTAINER_DISTRO_LC_NAME}${TARGET_BITS}"
+      fi
+    fi
+
+    local distribution_file="${DEPLOY_FOLDER_PATH}/${DISTRO_LC_NAME}-${APP_LC_NAME}-${distribution_file_version}-${target_folder_name}"
+
+    cd "${APP_PREFIX}"
+    find . -name '.DS_Store' -exec rm '{}' ';'
+
+    echo
+    echo "Creating distribution..."
+
+    mkdir -p "${DEPLOY_FOLDER_PATH}"
+
+    # The folder is temprarily moved into a a more elaborate hierarchy like
+    # gnu-mcu-eclipse/app-name/version.
+    # After the archive is created, the folders are moved back.
+    # The atempt to transform the tar path failes, since symlinks were
+    # also transformed, which is bad.
+    if [ "${TARGET_PLATFORM}" == "win32" ]
+    then
+
+      local distribution_file="${distribution_file}.zip"
+      local archive_version_path="${INSTALL_FOLDER_PATH}/archive/${DISTRO_UC_NAME}/${APP_UC_NAME}/${distribution_file_version}"
+
+      echo
+      echo "ZIP file: \"${distribution_file}\"."
+
+      rm -rf "${INSTALL_FOLDER_PATH}"/archive
+      mkdir -p "${archive_version_path}"
+      mv "${APP_PREFIX}"/* "${archive_version_path}"
+
+      cd "${INSTALL_FOLDER_PATH}"/archive
+      zip -r9 -q "${distribution_file}" *
+
+      # Put folders back.
+      mv "${archive_version_path}"/* "${APP_PREFIX}"
+
+    else
+
+      # Unfortunately on node.js, xz & bz2 require native modules, which
+      # proved unsafe, some xz versions failed to compile on node.js v9.x,
+      # so use the good old .tgz.
+      local distribution_file="${distribution_file}.tgz"
+      local archive_version_path="${INSTALL_FOLDER_PATH}/archive/${DISTRO_LC_NAME}/${APP_LC_NAME}/${distribution_file_version}"
+
+      echo "Compressed tarball: \"${distribution_file}\"."
+
+      rm -rf "${INSTALL_FOLDER_PATH}"/archive
+      mkdir -p "${archive_version_path}"
+      mv -v "${APP_PREFIX}"/* "${archive_version_path}"
+
+      # Without --hard-dereference the hard links may be turned into
+      # broken soft links on macOS.
+      cd "${INSTALL_FOLDER_PATH}"/archive
+      # -J uses xz for compression; best compression ratio.
+      # -j uses bz2 for compression; good compression ratio.
+      # -z uses gzip for compression; fair compression ratio.
+      tar -c -z -f "${distribution_file}" \
+        --owner=0 \
+        --group=0 \
+        --format=posix \
+        --hard-dereference \
+        *
+
+      # Put folders back.
+      mv -v "${archive_version_path}"/* "${APP_PREFIX}"
+
+    fi
+
+    cd "${DEPLOY_FOLDER_PATH}"
+    compute_sha shasum -a 256 -p "$(basename ${distribution_file})"
+  )
+}
+
+# -----------------------------------------------------------------------------
+
+function compute_sha() 
+{
+  # $1 shasum program
+  # $2.. options
+  # ${!#} file
+
+  file=${!#}
+  sha_file="${file}.sha"
+  "$@" >"${sha_file}"
+  echo "SHA: $(cat ${sha_file})"
+}
+
+
+# -----------------------------------------------------------------------------
+
 # Default empty definition, if XBB is available, it should
 # redefine it.
 function xbb_activate()
@@ -465,6 +923,7 @@ function xbb_activate()
   :
 }
 
+# Example, to fix the missing definitions on ARCH.
 function xbb_activate_pkgconfig()
 {
   if [ "${TARGET_PLATFORM}" == "linux" ]
@@ -485,20 +944,20 @@ function xbb_activate_includes()
   :
 }
 
-# Default, to fix the missing definition on ARCH.
-function xbb_activate_dev()
-{
-  xbb_activate_pkgconfig
-  xbb_activate_includes
-}
-
+# Make the build use the currently compiled libraries.
+# The pkg-config path will no longer include the system paths.
 function xbb_activate_this()
 {
   export EXTRA_CPPFLAGS+=" -I${LIBS_INSTALL_FOLDER_PATH}/include"
   export EXTRA_LDFLAGS+=" -L${LIBS_INSTALL_FOLDER_PATH}/lib"
   export EXTRA_LDFLAGS_APP+=" -L${LIBS_INSTALL_FOLDER_PATH}/lib"
 
-  export PKG_CONFIG_PATH="${LIBS_INSTALL_FOLDER_PATH}/lib/pkgconfig:${PKG_CONFIG_PATH}"
+  if [ "${TARGET_PLATFORM}" == "linux" -a "${TARGET_ARCH}" == "x64" ]
+  then
+    export PKG_CONFIG_PATH="${LIBS_INSTALL_FOLDER_PATH}/lib64/pkgconfig:${LIBS_INSTALL_FOLDER_PATH}/lib/pkgconfig"
+  else
+    export PKG_CONFIG_PATH="${LIBS_INSTALL_FOLDER_PATH}/lib/pkgconfig"
+  fi
 }
 
 # -----------------------------------------------------------------------------
