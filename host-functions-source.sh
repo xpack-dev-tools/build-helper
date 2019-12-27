44030936e1bd1cadeb5909b9f5513f1eb40894aa
Liviu Ionescu <ilg@livius.net>
2019-12-27 13:56:56 +0200

    host-functions: remove docker images
    - better have the application define them explicitly

--- host-functions-source.sh
+++ host-functions-source.sh
@@ -512,13 +512,6 @@ function host_common()
 
   SOURCES_FOLDER_PATH="${SOURCES_FOLDER_PATH:-"${HOST_WORK_FOLDER_PATH}/sources"}"
 
-  # The names of the two Docker images used for the build.
-  # docker run --interactive --tty ilegeul/centos:6-xbb-v2.1
-  docker_linux64_image=${docker_linux64_image:-"ilegeul/centos:6-xbb-v2.2"}
-  docker_linux32_image=${docker_linux32_image:-"ilegeul/centos32:6-xbb-v2.2"}
-  docker_linux_arm64_image=${docker_linux_arm64_image:-"ilegeul/ubuntu:arm64-16.04-xbb-v1.1"}
-  docker_linux_arm32_image=${docker_linux_arm32_image:-"ilegeul/ubuntu:armhf-16.04-xbb-v1.1"}
-
   do_actions
 
   host_prepare_cache
