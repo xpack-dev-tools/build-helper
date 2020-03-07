6ec4d50aab83eb6cb5f2eaa30b4ea847acc264e5
Liviu Ionescu <ilg@livius.net>
2020-03-07 22:39:38 +0200

    common-functions: export locale

--- common-functions-source.sh
+++ common-functions-source.sh
@@ -299,6 +299,16 @@ function prepare_xbb_env()
 
   # ---------------------------------------------------------------------------
 
+  export LANGUAGE="en_US:en"
+  export LANG="en_US.UTF-8"
+  export LC_ALL="en_US.UTF-8"
+  export LC_COLLATE="en_US.UTF-8"
+  export LC_CTYPE="UTF-8"
+  export LC_MESSAGES="en_US.UTF-8"
+  export LC_MONETARY="en_US.UTF-8"
+  export LC_NUMERIC="en_US.UTF-8"
+  export LC_TIME="en_US.UTF-8"
+
   export PATH
   export LD_LIBRARY_PATH
 
