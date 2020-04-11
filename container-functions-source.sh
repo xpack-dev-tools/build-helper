22eaab6018b8a53d1a4d381bbb2a2b0bfe8d0bd0
Liviu Ionescu <ilg@livius.net>
2020-04-11 17:16:06 +0300

    add copyright notice

--- container-functions-source.sh
+++ container-functions-source.sh
@@ -1,4 +1,11 @@
 # -----------------------------------------------------------------------------
+# This file is part of the xPack distribution.
+#   (https://xpack.github.io)
+# Copyright (c) 2020 Liviu Ionescu.
+#
+# Permission to use, copy, modify, and/or distribute this software 
+# for any purpose is hereby granted, under the terms of the MIT license.
+# -----------------------------------------------------------------------------
 
 # Helper script used in the second edition of the xPack build 
 # scripts. As the name implies, it should contain only functions and 
