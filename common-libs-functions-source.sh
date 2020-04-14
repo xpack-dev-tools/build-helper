041e00eb8ba79476ceb5993f74959723b64d3c20
Liviu Ionescu <ilg@livius.net>
2020-04-14 19:30:09 +0300

    common-libs: rework ncurses, use array and separate logs folder

--- common-libs-functions-source.sh
+++ common-libs-functions-source.sh
@@ -24,6 +24,10 @@ function do_ncurses()
   # https://archlinuxarm.org/packages/aarch64/ncurses/files/PKGBUILD
   # http://deb.debian.org/debian/pool/main/n/ncurses/ncurses_6.1+20181013.orig.tar.gz.asc
 
+  # https://github.com/msys2/MINGW-packages/blob/master/mingw-w64-ncurses/PKGBUILD
+  # https://github.com/msys2/MINGW-packages/blob/master/mingw-w64-ncurses/001-use-libsystre.patch
+  # https://github.com/msys2/MSYS2-packages/blob/master/ncurses/PKGBUILD
+
   # _4421.c:1364:15: error: expected ‘)’ before ‘int’
   # ../include/curses.h:1906:56: note: in definition of macro ‘mouse_trafo’
   # 1906 | #define mouse_trafo(y,x,to_screen) wmouse_trafo(stdscr,y,x,to_screen)
@@ -59,6 +63,8 @@ function do_ncurses()
       mkdir -pv "${LIBS_BUILD_FOLDER_PATH}/${ncurses_folder_name}"
       cd "${LIBS_BUILD_FOLDER_PATH}/${ncurses_folder_name}"
 
+      mkdir -pv "${LOGS_FOLDER_PATH}/${ncurses_folder_name}"
+
       xbb_activate
       xbb_activate_installed_dev
 
@@ -75,83 +81,97 @@ function do_ncurses()
 
           bash "${SOURCES_FOLDER_PATH}/${ncurses_src_folder_name}/configure" --help
 
+          config_options=()
+
           # Not yet functional on windows.
           if [ "${TARGET_PLATFORM}" == "win32" ]
           then
 
+            # The build passes, but generally it is not expected to be
+            # used on Windows.
+
             # export PATH_SEPARATOR=";"
+
+            # --with-libtool \
+            # /opt/xbb/bin/libtool: line 10548: gcc-8bs: command not found
 
             # Without --with-pkg-config-libdir= it'll try to write the .pc files in the
             # xbb folder, probbaly by using the dirname of pkg-config.
-            bash ${DEBUG} "${SOURCES_FOLDER_PATH}/${ncurses_src_folder_name}/configure" \
-              --prefix="${LIBS_INSTALL_FOLDER_PATH}" \
-              \
-              --build=${BUILD} \
-              --host=${HOST} \
-              --target=${TARGET} \
-              --with-build-cc=${CC} \
-              --with-build-cflags=${CFLAGS} \
-              --with-build-cppflags=${CPPFLAGS} \
-              --with-build-ldflags=${LDFLAGS} \
-              \
-              --with-shared \
-              --with-normal \
-              --with-cxx \
-              --with-cxx-binding \
-              --with-cxx-shared \
-              --with-pkg-config-libdir="${LIBS_INSTALL_FOLDER_PATH}/lib/pkgconfig" \
-              --without-ada \
-              --without-debug \
-              --without-manpage \
-              --without-prog \
-              \
-              --enable-assertions \
-              --enable-sp-funcs \
-              --enable-term-driver \
-              --enable-interop \
-              --enable-pc-files \
-              --disable-termcap \
-              --disable-home-terminfo \
+
+            config_options+=("--with-build-cc=${NATIVE_CC}")
+            config_options+=("--with-build-cflags=${CFLAGS}")
+            config_options+=("--with-build-cppflags=${CPPFLAGS}")
+            config_options+=("--with-build-ldflags=${LDFLAGS}")
+               
+            config_options+=("--without-progs")
+
+            # Only for the MinGW port, it provides a way to substitute
+            # the low-level terminfo library with different terminal drivers.
+            config_options+=("--enable-term-driver")
+            
+            config_options+=("--disable-termcap")
+            config_options+=("--disable-home-terminfo")
+            config_options+=("--disable-db-install")
 
           else
 
             # Without --with-pkg-config-libdir= it'll try to write the .pc files in the
             # xbb folder, probbaly by using the dirname of pkg-config.
-            bash ${DEBUG} "${SOURCES_FOLDER_PATH}/${ncurses_src_folder_name}/configure" \
-              --prefix="${LIBS_INSTALL_FOLDER_PATH}" \
-              \
-              --build=${BUILD} \
-              --host=${HOST} \
-              --target=${TARGET} \
-              \
-              --with-shared \
-              --with-normal \
-              --with-cxx \
-              --with-cxx-binding \
-              --with-cxx-shared \
-              --with-pkg-config-libdir="${LIBS_INSTALL_FOLDER_PATH}/lib/pkgconfig" \
-              --with-terminfo-dirs=/etc/terminfo \
-              --with-default-terminfo-dir="/etc/terminfo:/lib/terminfo:/usr/share/terminfo" \
-              --with-gpm \
-              --with-versioned-syms \
-              --with-xterm-kbs=del \
-              --without-debug \
-              --without-ada \
-              --without-manpage \
-              --without-prog \
-              \
-              --enable-widec \
-              --enable-pc-files \
-              --enable-termcap \
-              --enable-ext-colors \
-              --enable-const \
-              --enable-symlinks \
-              --enable-overwrite \
+
+            config_options+=("--with-terminfo-dirs=/etc/terminfo")
+            config_options+=("--with-default-terminfo-dir=/etc/terminfo:/lib/terminfo:/usr/share/terminfo")
+            config_options+=("--with-gpm")
+            config_options+=("--with-versioned-syms")
+            config_options+=("--with-xterm-kbs=del")
+
+            config_options+=("--enable-termcap")
+            config_options+=("--enable-const")
+            config_options+=("--enable-symlinks")
 
           fi
 
-          cp "config.log" "${LOGS_FOLDER_PATH}/config-ncurses-log.txt"
-        ) 2>&1 | tee "${LOGS_FOLDER_PATH}/configure-ncurses-output.txt"
+          config_options+=("--with-shared")
+          config_options+=("--with-normal")
+          config_options+=("--with-cxx")
+          config_options+=("--with-cxx-binding")
+          config_options+=("--with-cxx-shared")
+          config_options+=("--with-pkg-config-libdir=${LIBS_INSTALL_FOLDER_PATH}/lib/pkgconfig")
+          
+          # Fails on Linux, with missing _nc_cur_term, which is there.
+          config_options+=("--without-pthread")
+
+          config_options+=("--without-ada")
+          config_options+=("--without-debug")
+          config_options+=("--without-manpages")
+          config_options+=("--without-tack")
+          config_options+=("--without-tests")
+
+          config_options+=("--enable-pc-files")
+          config_options+=("--enable-sp-funcs")
+          config_options+=("--enable-ext-colors")
+          config_options+=("--enable-interop")
+
+          config_options+=("--disable-lib-suffixes")
+          config_options+=("--disable-overwrite")
+
+          NCURSES_DISABLE_WIDEC=${NCURSES_DISABLE_WIDEC:-""}
+
+          if [ "${NCURSES_DISABLE_WIDEC}" == "y" ]
+          then
+            config_options+=("--disable-widec")
+          fi
+
+          bash ${DEBUG} "${SOURCES_FOLDER_PATH}/${ncurses_src_folder_name}/configure" \
+            --prefix="${LIBS_INSTALL_FOLDER_PATH}" \
+            \
+            --build=${BUILD} \
+            --host=${HOST} \
+            --target=${TARGET} \
+            \
+            ${config_options[@]}
+
+          cp "config.log" "${LOGS_FOLDER_PATH}/${ncurses_folder_name}/config-log.txt"
+        ) 2>&1 | tee "${LOGS_FOLDER_PATH}/${ncurses_folder_name}/configure-output.txt"
       fi
 
       (
@@ -186,7 +206,7 @@ function do_ncurses()
         rm -f "${LIBS_INSTALL_FOLDER_PATH}/lib/libcurses.so"
         ln -s -v libncurses.so "${LIBS_INSTALL_FOLDER_PATH}/lib/libcurses.so"
 
-      ) 2>&1 | tee "${LOGS_FOLDER_PATH}/make-ncurses-output.txt"
+      ) 2>&1 | tee "${LOGS_FOLDER_PATH}/${ncurses_folder_name}/make-output.txt"
 
       copy_license \
         "${SOURCES_FOLDER_PATH}/${ncurses_src_folder_name}" \
