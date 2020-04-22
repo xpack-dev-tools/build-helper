c3dcea43d58c92f79b7013363d4e3f89d8b5b47b
Liviu Ionescu <ilg@livius.net>
2020-04-22 21:50:38 +0300

    common-libs: use config_options in mpc

--- common-libs-functions-source.sh
+++ common-libs-functions-source.sh
@@ -400,12 +400,16 @@ function do_mpc()
         
           bash "${SOURCES_FOLDER_PATH}/${mpc_src_folder_name}/configure" --help
 
+          config_options=()
+
+          config_options+=("--prefix=${LIBS_INSTALL_FOLDER_PATH}")
+            
+          config_options+=("--build=${BUILD}")
+          config_options+=("--host=${HOST}")
+          config_options+=("--target=${TARGET}")
+
           bash ${DEBUG} "${SOURCES_FOLDER_PATH}/${mpc_src_folder_name}/configure" \
-            --prefix="${LIBS_INSTALL_FOLDER_PATH}" \
-            \
-            --build=${BUILD} \
-            --host=${HOST} \
-            --target=${TARGET} \
+            ${config_options[@]}
             
           cp "config.log" "${LOGS_FOLDER_PATH}/${mpc_folder_name}/config-log.txt"
         ) 2>&1 | tee "${LOGS_FOLDER_PATH}/${mpc_folder_name}/configure-output.txt"
