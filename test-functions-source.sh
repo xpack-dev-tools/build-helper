e95854cf79de3883cf771528401baa67787a6386
Liviu Ionescu <ilg@livius.net>
2020-10-11 11:42:03 +0300

    test-functions: prepare nvm environment

--- test-functions-source.sh
+++ test-functions-source.sh
@@ -1203,6 +1203,10 @@ function install_xpm()
 {
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
   
+  export NVM_DIR="${HOME}/.nvm"
+  [ -s "${NVM_DIR}/nvm.sh" ] && \. "${NVM_DIR}/nvm.sh"  # This loads nvm
+  [ -s "${NVM_DIR}/bash_completion" ] && \. "${NVM_DIR}/bash_completion"  # This loads nvm bash_completion
+
   nvm install --lts node
   nvm use --lts node
 
