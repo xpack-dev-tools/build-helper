f8e922f7b174685455cd85824f55d5598214db0c
Liviu Ionescu <ilg@livius.net>
2021-09-24 11:57:13 +0300

    container-build: temporary hack texlive path

--- container-build.sh
+++ container-build.sh
@@ -75,6 +75,18 @@ source "${scripts_folder_path}/common-versions-source.sh"
 
 # -----------------------------------------------------------------------------
 
+# Temporary hack.
+export TEXLIVE_FOLDER_PATH="/opt/texlive"
+
+function xbb_activate_tex()
+{
+  PATH="${TEXLIVE_FOLDER_PATH}/bin/$(ls "${TEXLIVE_FOLDER_PATH}/bin" | sed -n -e 1p):${PATH}"
+
+  export PATH
+}
+
+# -----------------------------------------------------------------------------
+
 if [ ! -z "#{DEBUG}" ]
 then
   echo $@
