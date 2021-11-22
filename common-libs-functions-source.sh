992a480ba99fd73d0d614e0a81e38528cf624a80
Liviu Ionescu <ilg@livius.net>
2021-11-22 19:47:15 +0200

    common-libs: disable isl tests for aarch64 too

--- common-libs-functions-source.sh
+++ common-libs-functions-source.sh
@@ -699,13 +699,14 @@ function build_isl()
 
         if [ "${WITH_TESTS}" == "y" ]
         then
-          if [ "${TARGET_PLATFORM}" == "linux" -a \
-            \( "${TARGET_ARCH}" == "x64" -o "${TARGET_ARCH}" == "x32" -o "${TARGET_ARCH}" == "ia32" \) ] 
+          if [ "${TARGET_PLATFORM}" == "linux" ] 
           then
             # /Host/Users/ilg/Work/gcc-8.4.0-1/linux-x64/build/libs/isl-0.22/.libs/lt-isl_test_cpp: relocation error: /Host/Users/ilg/Work/gcc-8.4.0-1/linux-x64/build/libs/isl-0.22/.libs/lt-isl_test_cpp: symbol _ZNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEE9_M_createERmm, version GLIBCXX_3.4.21 not defined in file libstdc++.so.6 with link time reference
             # FAIL isl_test_cpp (exit status: 127)
             # /Host/Users/ilg/Work/gcc-8.4.0-1/linux-x32/build/libs/isl-0.22/.libs/lt-isl_test_cpp: relocation error: /Host/Users/ilg/Work/gcc-8.4.0-1/linux-x32/build/libs/isl-0.22/.libs/lt-isl_test_cpp: symbol _ZNSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEE9_M_createERjj, version GLIBCXX_3.4.21 not defined in file libstdc++.so.6 with link time reference
             # FAIL isl_test_cpp (exit status: 127)
+            # Similar for aarch64
+            # FAIL: isl_test_cpp
 
             run_verbose make -j1 check || true
           else
