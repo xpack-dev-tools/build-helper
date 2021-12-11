cd52ad39a155f90c75adeba51ffd8d593a0bd136
Liviu Ionescu <ilg@livius.net>
2021-12-12 01:00:34 +0200

    common-libs: add build_libcap

--- common-libs-functions-source.sh
+++ common-libs-functions-source.sh
@@ -5937,3 +5937,117 @@ function build_vde()
 
 # -----------------------------------------------------------------------------
 
+function build_libpcap()
+{
+  # Portable library for network traffic capture
+  # https://www.tcpdump.org/
+  # https://www.tcpdump.org/release/
+  # https://www.tcpdump.org/release/libpcap-1.10.1.tar.gz
+
+  # https://github.com/archlinux/svntogit-packages/blob/packages/libpcap/trunk/PKGBUILD
+
+  # https://github.com/Homebrew/homebrew-core/blob/master/Formula/libpcap.rb
+
+  # June 9, 2021 "1.10.1"
+
+  local libpcap_version="$1"
+
+  local libpcap_src_folder_name="libpcap-${libpcap_version}"
+
+  local libpcap_archive="${libpcap_src_folder_name}.tar.gz"
+  local libpcap_url="https://www.tcpdump.org/release/${libpcap_archive}"
+
+  local libpcap_folder_name="${libpcap_src_folder_name}"
+
+  mkdir -pv "${LOGS_FOLDER_PATH}/${libpcap_folder_name}"
+
+  local libpcap_stamp_file_path="${STAMPS_FOLDER_PATH}/stamp-${libpcap_folder_name}-installed"
+  if [ ! -f "${libpcap_stamp_file_path}" ]
+  then
+
+    cd "${SOURCES_FOLDER_PATH}"
+
+    download_and_extract "${libpcap_url}" "${libpcap_archive}" \
+      "${libpcap_src_folder_name}"
+
+    (
+      mkdir -pv "${LIBS_BUILD_FOLDER_PATH}/${libpcap_folder_name}"
+      cd "${LIBS_BUILD_FOLDER_PATH}/${libpcap_folder_name}"
+
+      xbb_activate_installed_dev
+
+      CPPFLAGS="${XBB_CPPFLAGS}"
+      CFLAGS="${XBB_CFLAGS_NO_W}"
+      CXXFLAGS="${XBB_CXXFLAGS_NO_W}"
+
+      LDFLAGS="${XBB_LDFLAGS_LIB}"
+      if [ "${TARGET_PLATFORM}" == "linux" ]
+      then
+        LDFLAGS+=" -Wl,-rpath,${LD_LIBRARY_PATH}"
+      fi
+
+      export CPPFLAGS
+      export CFLAGS
+      export CXXFLAGS
+      export LDFLAGS
+
+      if [ ! -f "config.status" ]
+      then
+        (
+          if [ "${IS_DEVELOP}" == "y" ]
+          then
+            env | sort
+          fi
+
+          echo
+          echo "Running libpcap configure..."
+
+          if [ "${IS_DEVELOP}" == "y" ]
+          then
+            run_verbose bash "${SOURCES_FOLDER_PATH}/${libpcap_src_folder_name}/configure" --help
+          fi
+
+          config_options=()
+
+          config_options+=("--prefix=${LIBS_INSTALL_FOLDER_PATH}")
+
+          config_options+=("--build=${BUILD}")
+          config_options+=("--host=${HOST}")
+          config_options+=("--target=${TARGET}")
+
+          # HomeBrew
+          config_options+=("--disable-universal")
+
+          run_verbose bash ${DEBUG} "${SOURCES_FOLDER_PATH}/${libpcap_src_folder_name}/configure" \
+            "${config_options[@]}"
+
+          cp "config.log" "${LOGS_FOLDER_PATH}/${libpcap_folder_name}/config-log-$(ndate).txt"
+        ) 2>&1 | tee "${LOGS_FOLDER_PATH}/${libpcap_folder_name}/configure-output-$(ndate).txt"
+      fi
+
+      (
+        echo
+        echo "Running libpcap make..."
+
+        # Build.
+        run_verbose make -j ${JOBS}
+
+        run_verbose make install
+
+      ) 2>&1 | tee "${LOGS_FOLDER_PATH}/${libpcap_folder_name}/make-output-$(ndate).txt"
+
+      copy_license \
+        "${SOURCES_FOLDER_PATH}/${libpcap_src_folder_name}" \
+        "${libpcap_folder_name}"
+
+    )
+
+    touch "${libpcap_stamp_file_path}"
+
+  else
+    echo "Library libpcap already installed."
+  fi
+}
+
+# -----------------------------------------------------------------------------
+
